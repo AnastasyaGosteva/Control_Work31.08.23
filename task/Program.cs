a07f0482fa5fc4245416d5463a3fdbf6a9e1bcc2
Roman & Anastasiya <roma.gostev2017@yandex.ru>
2023-09-02 16:37:05 +0700

    creating a method CalculatinNumberMatchingWords

---
+++ task/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+void CalculatinNumberMatchingWords(string [] text, out int count)
+{
+    count = 0;
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i].Length <= 3)
+            count++;
+    }
+}
+
+string [] text = {"Роман", "Ян", "Ира", "Анастасия", "Оля", "Константин"};
+CalculatinNumberMatchingWords(text, out int count);
