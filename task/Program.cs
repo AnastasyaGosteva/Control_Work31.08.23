702f2628c1956249ce8f6a1fbb84198e00e0b531
Roman & Anastasiya <roma.gostev2017@yandex.ru>
2023-09-02 16:41:09 +0700

    creating a method GetNewArray

--- task/Program.cs
+++ task/Program.cs
@@ -1,4 +1,20 @@
 ﻿using System.Linq.Expressions;
+
+string[] GetNewArray(int count, string [] text)
+{
+    string[] newArray = new string[count];
+    int index = 0;
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i].Length <= 3)
+        {
+            newArray[index] = text[i];
+            index++;
+        }
+    }
+    return newArray;
+}
+
 void CalculatinNumberMatchingWords(string [] text, out int count)
 {
     count = 0;
@@ -11,3 +27,4 @@ void CalculatinNumberMatchingWords(string [] text, out int count)
 
 string [] text = {"Роман", "Ян", "Ира", "Анастасия", "Оля", "Константин"};
 CalculatinNumberMatchingWords(text, out int count);
+string [] textNew = GetNewArray(count, text);
